007b9b8417c6827d03e9e5ab83a4eea8f5881d24
Tatsuya9156 <hmty9156@gmail.com>
2021-01-06 20:56:44 +0900

    passwordのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email,               uniqueness: { case_sensitive: true }
-  validates :password,            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :email,    uniqueness: { case_sensitive: true }
+  validates :password, format: { with: /\A(?=.*?[A-z\d])[A-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
     validates :birth
