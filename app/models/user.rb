15bd346134b793edda1a1d158ca54e01f058a27d
Tatsuya9156 <hmty9156@gmail.com>
2021-01-06 13:37:45 +0900

    バリデーションが上手くいかないので一旦コメントアウト、それとbinding.pryの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :nickname
-    validates :email,               uniqueness: true
-    validates :encrypted_password,  inclusion: { in: %w(@)}, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
-      validates :last_name
-      validates :first_name
-    end
-    with_options format: { with: /\A[ァ-ヶ]+\z/ } do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
-    validates :birth
-  end
+  # with_options presence: true do
+  #   validates :nickname
+  #   validates :email               inclusion: { in: %w(@)}, uniqueness: { case_sensitive: true }
+  #   validates :encrypted_password,  length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  #   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+  #     validates :last_name
+  #     validates :first_name
+  #   end
+  #   with_options format: { with: /\A[ァ-ヶ]+\z/ } do
+  #     validates :last_name_kana
+  #     validates :first_name_kana
+  #   end
+  #   validates :birth
+  # end
+
 end
