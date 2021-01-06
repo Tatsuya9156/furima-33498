c924099e2e8c9bfc164ec5e92a6a210d4d6cfb57
Tatsuya9156 <hmty9156@gmail.com>
2021-01-06 14:35:12 +0900

    バリデーションの再設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # with_options presence: true do
-  #   validates :nickname
-  #   validates :email               inclusion: { in: %w(@)}, uniqueness: { case_sensitive: true }
-  #   validates :encrypted_password,  length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  #   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
-  #     validates :last_name
-  #     validates :first_name
-  #   end
-  #   with_options format: { with: /\A[ァ-ヶ]+\z/ } do
-  #     validates :last_name_kana
-  #     validates :first_name_kana
-  #   end
-  #   validates :birth
-  # end
+  validates :email,               uniqueness: { case_sensitive: true }
+  validates :password,            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true do
+    validates :nickname
+    validates :birth
+    with_options format: { with: /\A[ァ-ヶ]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+      with_options format: { with: /\A[ぁ-ん一-龥々]+\z/ } do
+        validates :last_name
+        validates :first_name
+      end
+    end
+  end
 
 end
