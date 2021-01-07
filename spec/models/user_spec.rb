ff7196db4e7eeda74cefbccb32052ae3d3d710ef
Tatsuya9156 <hmty9156@gmail.com>
2021-01-07 11:30:27 +0900

    パスワードに関するテストコードを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,6 +101,21 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
+      it 'passwordが英語のみでは登録できない' do
+        @user.password = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it 'passwordが数字のみでは登録できない' do
+        @user.password = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it 'passwordが全角では登録できない' do
+        @user.password = 'ａｂｃ１２３'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password_confirmation = 'test123'
         @user.valid?
