5f1814a2a17addddf84ac8f5766d7d62f841d96c
Tatsuya9156 <hmty9156@gmail.com>
2021-01-07 19:12:46 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    elsif
+      render :new
+    end
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :ship_cost_id, :prefecture_id, :ship_date_id, :price).merge(user_id: current_user.id)
+  end
 end
