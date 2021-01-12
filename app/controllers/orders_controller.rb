2694b994f2d3d42eba25b4d472b2d699b33b9296
Tatsuya9156 <hmty9156@gmail.com>
2021-01-12 11:12:17 +0900

    重複コードのまとめ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :find_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       @order_address.save
@@ -20,4 +20,7 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:zip, :prefecture_id, :city, :block, :building, :phone).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
 end
