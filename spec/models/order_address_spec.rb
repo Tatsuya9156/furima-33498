734f6dd357e056e7d4a9adff9393b1fe869eed43
Tatsuya9156 <hmty9156@gmail.com>
2021-01-12 18:47:06 +0900

    単体テストコードの修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -70,6 +70,21 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include('Phone is invalid')
       end
+      it 'phoneは数字のみでないと購入できない' do
+        @order_address.phone = 'abcde12345'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include('Phone is invalid')
+      end
+      it 'item_idが空では購入できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'user_idが空では購入できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
     end
   end
 end
