8a7a46d9caba23e81b4f906f4e49056b94f0dbd4
Tatsuya9156 <hmty9156@gmail.com>
2021-01-12 09:42:48 +0900

    バリデーションの設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,9 +2,18 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :zip, :prefecture_id, :city, :block, :building, :phone, :item_id, :user_id, :token
 
+  with_options presence: true do
+    validates :zip,           format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :block
+    validates :phone,         numericality: { only_integer: true }, format: { with: /\A[0-9]{,11}\z/ }
+    validates :token
+  end
+
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    
+
     Address.create(zip: zip, prefecture_id: prefecture_id, city: city, block: block, building: building, phone: phone, order_id: order.id)
 
     item = Item.find(item_id)
